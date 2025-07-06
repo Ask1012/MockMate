cb031c99fe8bf454f5e286ecce041c8dd70ba50a
Ask1012 <ak1074834@gmail.com>
2025-07-06 13:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def contact():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
